120efef0d30b91da7a2e3dc8e6dd1f9dc9a1c3e4
VAIO\Minhq <minhquan482212@gmail.com>
2023-10-23 21:57:35 +0700

    Update Ex1

--- MCU_LAB/Core/Src/main.c
+++ MCU_LAB/Core/Src/main.c
@@ -56,7 +56,18 @@ static void MX_TIM2_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+void display(int val){
+	switch (val){
+		case 1:
+			HAL_GPIO_WritePin(GPIOB, SEG1_Pin | SEG2_Pin , RESET);
+			HAL_GPIO_WritePin(GPIOB, SEG0_Pin | SEG3_Pin | SEG4_Pin | SEG5_Pin| SEG6_Pin , SET);
+			break;
+		case 2:
+			HAL_GPIO_WritePin(GPIOB, SEG0_Pin | SEG1_Pin | SEG3_Pin | SEG4_Pin | SEG6_Pin , RESET);
+			HAL_GPIO_WritePin(GPIOB, SEG2_Pin | SEG5_Pin , SET);
+			break;
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -95,14 +106,10 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  setTimer1(200);
   while (1)
   {
     /* USER CODE END WHILE */
-	  if(timer1_flag == 1){
-		  setTimer1(200);
-		  HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-	  }
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -199,22 +206,59 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|EN0_Pin|EN1_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : LED_RED_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin;
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, SEG0_Pin|SEG1_Pin|SEG2_Pin|SEG3_Pin
+                          |SEG4_Pin|SEG5_Pin|SEG6_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_RED_Pin EN0_Pin EN1_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|EN0_Pin|EN1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_RED_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SEG0_Pin SEG1_Pin SEG2_Pin SEG3_Pin
+                           SEG4_Pin SEG5_Pin SEG6_Pin */
+  GPIO_InitStruct.Pin = SEG0_Pin|SEG1_Pin|SEG2_Pin|SEG3_Pin
+                          |SEG4_Pin|SEG5_Pin|SEG6_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
 /* USER CODE BEGIN 4 */
-int counter = 100;
+int counter = 50;
+int status = 1;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+	if(counter > 0){
+		counter--;
+		if(counter <= 0){
+			switch(status){
+			case 1:
+				status = 2;
+				HAL_GPIO_WritePin(GPIOA, EN0_Pin, RESET);
+				HAL_GPIO_WritePin(GPIOA, EN1_Pin, SET);
+				display(1);
+				break;
+			case 2:
+				status = 1;
+				HAL_GPIO_WritePin(GPIOA, EN1_Pin, RESET);
+				HAL_GPIO_WritePin(GPIOA, EN0_Pin, SET);
+				display(2);
+				break;
+			}
+			HAL_GPIO_TogglePin(GPIOA, LED_RED_Pin);
+			counter = 100;
+		}
+	}
+
 	timerRun();
 }
 /* USER CODE END 4 */
